936e0a9eba77b0289ed809a9e130579b47f86783
Dylan Bonner <dbonner2@huskers.unl.edu>
2021-08-25 20:27:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/** Author: Dylan Bonner
+ * Date: 8/25/2021
+ * Hack 1.0
+ **/
+ 
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dylan Bonner\n");
+
+  return 0;
+}
